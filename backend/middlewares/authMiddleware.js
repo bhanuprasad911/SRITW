a13ea838af6852ed1070f2979b7b1458336a0811
bhanuprasad9111 <bhanuprasadchintu.170403@gmail.com>
2025-06-03 02:11:36 +0530

    edited

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -7,8 +7,7 @@ const authMiddleWare = async (req, res, next) => {
     const token = req.cookies.token;
     if (!token) return res.status(401).json({ message: "Unauthorized" });
     const decoded = jwt.verify(token, secret);
-    console.log(decoded)
-    const user = await Student.findOne(decoded._id).select("-password");
+    const user = await Student.findOne({id:decoded.id}).select("-password");
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
@@ -16,7 +15,7 @@ const authMiddleWare = async (req, res, next) => {
     next();
   } catch (error) {
     console.log(error.message);
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Error" })
   }
 };
 export default authMiddleWare;
