43b7c70acc0af02e40f304438935e7588872d521
bhanuprasad9111 <bhanuprasadchintu.170403@gmail.com>
2025-06-03 02:02:37 +0530

    auth

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { login, me, signup } from '../controllers/authControllers.js';
+import authMiddleWare from '../middlewares/authMiddleware.js';
+
+
+const authRouter = express.Router()
+authRouter.post('/signup', signup)
+authRouter.post('/login', login)
+authRouter.get('/me', authMiddleWare, me)
+
+
+export default authRouter;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,32 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import express from 'express'
-import cors from 'cors'
-import cookieparser from 'cookie-parser'
-import dbConnection from './configs/dbConnection.js'
-const app=express()
-app.use(cors(
-    {
-        origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials:true
-        }
-))
-app.use(express.json())
-app.use(cookieparser())
+import dotenv from 'dotenv';
+dotenv.config();
 
-app.listen(5000,function(){
-    console.log("server")
-    dbConnection()
-})
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dbConnection from '../src/configs/dbConnection.js'
+import authRouter from '../routes/auth.route.js'; 
+
+const app = express();
+
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use('/api/auth', authRouter);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  dbConnection();
+});
